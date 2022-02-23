1123df3933ffe6d304847d69f59eca1e2874767f
Dhovin <4074997+Dhovin@users.noreply.github.com>
2022-02-23 12:25:43 -0600

    Update updater.ps1

--- updater.ps1
+++ updater.ps1
@@ -10,13 +10,13 @@ $password = Read-host "Enter password"
 $alephium = wget "https://whattomine.com/coins/349.json?hr=1.0&p=0.0&fee=0.0&cost=0.0&hcost=0.0&span_br=1h&span_d=24" | ConvertFrom-Json
 $etherium = wget "https://whattomine.com/coins/151.json?hr=1000.0&p=0.0&fee=0.0&cost=0.0&hcost=0.0&span_br=1h&span_d=24" | ConvertFrom-Json
 
-$ALPHcoefficient = ([decimal]$alephium.profit.Replace("$",""))/1000
-$ETHcoefficient = ([decimal]$etherium.profit.Replace("$",""))/1000
+$ALPHppMH = ([decimal]$alephium.profit.Replace("$",""))/1000
+$ETHppMH = ([decimal]$etherium.profit.Replace("$",""))/1000
 
 #Login to T-Rex Miner
 $sid = Invoke-WebRequest -Uri "http://$ipaddress`:4067/login?password=$password" | ConvertFrom-Json
 #updates profit-per-mh
-Invoke-RestMethod -Method POST -Body "{`"profit_per_mh`":`"$ETHcoefficient`:$ALPHcoefficient`",`"sid`":`"$($sid.sid)`"}" -Uri "http://192.168.10.49:4067/config"
+Invoke-RestMethod -Method POST -Body "{`"profit_per_mh`":`"$ETHppMH`:$ALPHppMH`",`"sid`":`"$($sid.sid)`"}" -Uri "http://192.168.10.49:4067/config"
 #restart T-Rex Miner (unfortunately required to update)
 Invoke-WebRequest -Uri "http://$ipaddress`:4067/control?command=restart&sid=$($sid.sid)"
 #logs out 
